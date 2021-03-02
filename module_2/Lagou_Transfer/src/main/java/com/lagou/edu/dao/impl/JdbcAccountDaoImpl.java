22ce27be30232ae96dc488426db235b7e6e7c85d
zhouchaowei <zhouchaowei96492@163.com>
2021-03-03 00:13:17 +0800

    将数据库连接与当前线程进行绑定

--- module_2/Lagou_Transfer/src/main/java/com/lagou/edu/dao/impl/JdbcAccountDaoImpl.java
+++ module_2/Lagou_Transfer/src/main/java/com/lagou/edu/dao/impl/JdbcAccountDaoImpl.java
@@ -2,6 +2,7 @@ package com.lagou.edu.dao.impl;
 
 import com.lagou.edu.pojo.Account;
 import com.lagou.edu.dao.AccountDao;
+import com.lagou.edu.utils.ConnectionUtils;
 import com.lagou.edu.utils.DruidUtils;
 
 import java.sql.Connection;
@@ -16,9 +17,12 @@ public class JdbcAccountDaoImpl implements AccountDao {
 
     @Override
     public Account queryAccountByCardNo(String cardNo) throws Exception {
-        Connection con = DruidUtils.getInstance().getConnection();
+        /** 传统方式获取数据库连接 */
+//        Connection con = DruidUtils.getInstance().getConnection();
+        /** 从当前线程中获取绑定的数据库连接(不能关闭该连接) */
+        Connection connection = ConnectionUtils.getInstance().getConnectionFromCurrentThread();
         String sql = "select * from ACCOUNT where account_card_no = ?";
-        PreparedStatement preparedStatement = con.prepareStatement(sql);
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1,cardNo);
         ResultSet resultSet = preparedStatement.executeQuery();
 
@@ -36,9 +40,12 @@ public class JdbcAccountDaoImpl implements AccountDao {
 
     @Override
     public int updateAccountByCardNo(Account account) throws Exception {
-        Connection con = DruidUtils.getInstance().getConnection();
+        /** 传统方式获取数据库连接 */
+//        Connection con = DruidUtils.getInstance().getConnection();
+        /** 从当前线程中获取绑定的数据库连接(不能关闭该连接) */
+        Connection connection = ConnectionUtils.getInstance().getConnectionFromCurrentThread();
         String sql = "update ACCOUNT set balance = ? where account_card_no = ?";
-        PreparedStatement preparedStatement = con.prepareStatement(sql);
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setInt(1,account.getBalance());
         preparedStatement.setString(2,account.getAccountCardNo());
         int i = preparedStatement.executeUpdate();
--- module_2/Lagou_Transfer/src/main/java/com/lagou/edu/utils/ConnectionUtils.java
+++ module_2/Lagou_Transfer/src/main/java/com/lagou/edu/utils/ConnectionUtils.java
@@ -1,27 +1,42 @@
 package com.lagou.edu.utils;
 
+import com.alibaba.druid.pool.DruidPooledConnection;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
+ * JDBC连接工具类
+ * (为实现事务控制,将当前线程与当前数据库操作所用连接进行绑定,使得当前线程获取绑定的Connection连接)
  * @author zhouchaowei
  */
 public class ConnectionUtils {
 
-    /** 存储当前线程的连接 */
-    private ThreadLocal<Connection> threadLocal = new ThreadLocal<>();
+    /** 用于存储当前线程的Connection连接 */
+    private ThreadLocal<Connection> connectionThreadLocal = new ThreadLocal<>();
+
+    /**
+     * 以下代码用于保证Connection单例
+     */
+    private static ConnectionUtils connectionUtils = new ConnectionUtils();
+
+    private ConnectionUtils() { }
+
+    public static ConnectionUtils getInstance(){
+        return connectionUtils;
+    }
 
     /**
      * 从当前线程获取连接
+     * (判断当前线程中是否已经绑定Connection连接,如果没有绑定,需要从Druid连接池中获取一个连接并绑定到当前线程中)
+     * @return
      */
-    public Connection getCurrentThreadConn() throws SQLException {
-        /** 判断当前线程中是否已经绑定连接，如果没有绑定，需要从连接池获取一个连接绑定到当前线程 */
-        Connection connection = threadLocal.get();
-        if(connection == null) {
-            // 从连接池拿连接并绑定到线程
+    public Connection getConnectionFromCurrentThread() throws SQLException {
+        Connection connection = connectionThreadLocal.get();
+        /** 如果当前线程中没有绑定Connection连接,则获取连接并进行绑定 */
+        if(connection == null){
             connection = DruidUtils.getInstance().getConnection();
-            // 绑定到当前线程
-            threadLocal.set(connection);
+            connectionThreadLocal.set(connection);
         }
         return connection;
     }
